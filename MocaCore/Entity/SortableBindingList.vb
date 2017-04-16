8983fb61df38591cba48f9dcb2a8f595192ed25e
MiYABiS <miyabis@live.jp>
2017-04-16 21:11:12 +0900

    #9, #10

---
+++ MocaCore/Entity/SortableBindingList.vb
@@ -0,0 +1,137 @@
+﻿
+Imports System.ComponentModel
+
+''' <summary>
+''' ソート用のバインディングリスト
+''' </summary>
+''' <typeparam name="T"></typeparam>
+Public Class SortableBindingList(Of T)
+    Inherits BindingList(Of T)
+
+    Private _isSortedValue As Boolean
+    Private _sortDirectionValue As ListSortDirection
+    Private _sortPropertyValue As PropertyDescriptor
+
+#Region " コンストラクタ "
+
+    ''' <summary>
+    ''' コンストラクタ
+    ''' </summary>
+    Public Sub New()
+    End Sub
+
+    ''' <summary>
+    ''' コンストラクタ
+    ''' </summary>
+    ''' <param name="list"></param>
+    Public Sub New(list As IList(Of T))
+        MyBase.New(list)
+        'For Each o As Object In list
+        '    Me.Add(DirectCast(o, T))
+        'Next
+    End Sub
+
+#End Region
+
+#Region " Overrides "
+
+    ''' <summary>
+    ''' ソート処理
+    ''' </summary>
+    ''' <param name="prop"></param>
+    ''' <param name="direction"></param>
+    Protected Overrides Sub ApplySortCore(prop As PropertyDescriptor, direction As ListSortDirection)
+        ' 並べ替えるリストを取得します。
+        Dim items As List(Of T) = TryCast(Me.Items, List(Of T))
+
+        ' 並べ替えるアイテムがある場合は、sort を適用して設定します。
+        If Not (items Is Nothing) Then
+            Dim pc As New PropertyComparer(Of T)(prop, direction)
+            items.Sort(pc)
+
+            _isSortedValue = True
+        Else
+            _isSortedValue = False
+        End If
+
+        ' バインドされたコントロールに表示を更新するように指示します。
+        Me.OnListChanged(New ListChangedEventArgs(ListChangedType.Reset, -1))
+    End Sub
+
+    ''' <summary>
+    ''' ソート対象
+    ''' </summary>
+    ''' <returns></returns>
+    Protected Overrides ReadOnly Property SortPropertyCore() As PropertyDescriptor
+        Get
+            Return _sortPropertyValue
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' ソート方向
+    ''' </summary>
+    ''' <returns></returns>
+    Protected Overrides ReadOnly Property SortDirectionCore() As ListSortDirection
+        Get
+            Return _sortDirectionValue
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' ソートのサポート
+    ''' 
+    ''' </summary>
+    ''' <returns></returns>
+    Protected Overrides ReadOnly Property SupportsSortingCore() As Boolean
+        Get
+            Return True
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' ソート済かどうか
+    ''' </summary>
+    ''' <returns></returns>
+    Protected Overrides ReadOnly Property IsSortedCore() As Boolean
+        Get
+            Return _isSortedValue
+        End Get
+    End Property
+
+#End Region
+
+End Class
+
+
+'汎用コンペアラークラス
+Public Class PropertyComparer(Of T)
+    Implements IComparer(Of T)
+
+    Public Sub New(prop As PropertyDescriptor, direction As ListSortDirection)
+        Me.prop = prop
+        sortDirection = If((direction = ListSortDirection.Ascending), 1, -1)
+    End Sub
+
+    Private prop As PropertyDescriptor
+    Private sortDirection As Integer
+
+
+    Private Function IComparer_Compare(x As T, y As T) As Integer Implements IComparer(Of T).Compare
+        Dim left As IComparable = TryCast(prop.GetValue(x), IComparable)
+        Dim right As IComparable = TryCast(prop.GetValue(y), IComparable)
+
+        Dim result As Integer
+
+        If Not (left Is Nothing) Then
+            result = left.CompareTo(right)
+        ElseIf right Is Nothing Then
+            result = 0
+        Else
+            result = -1
+        End If
+
+        Return result * sortDirection
+    End Function
+
+End Class
---
+++ MocaCore35/Entity/SortableBindingList.vb
@@ -0,0 +1,119 @@
+﻿
+Imports System.ComponentModel
+
+''' <summary>
+''' ソート用のバインディングリスト
+''' </summary>
+''' <typeparam name="T"></typeparam>
+Public Class SortableBindingList(Of T)
+    Inherits BindingList(Of T)
+
+    Private _isSortedValue As Boolean
+    Private _sortDirectionValue As ListSortDirection
+    Private _sortPropertyValue As PropertyDescriptor
+
+#Region " コンストラクタ "
+
+    ''' <summary>
+    ''' コンストラクタ
+    ''' </summary>
+    Public Sub New()
+    End Sub
+
+    ''' <summary>
+    ''' コンストラクタ
+    ''' </summary>
+    ''' <param name="list"></param>
+    Public Sub New(list As IList(Of T))
+        MyBase.New(list)
+    End Sub
+
+#End Region
+
+#Region " Overrides "
+
+    ''' <summary>
+    ''' ソート処理
+    ''' </summary>
+    ''' <param name="prop"></param>
+    ''' <param name="direction"></param>
+    Protected Overrides Sub ApplySortCore(prop As PropertyDescriptor, direction As ListSortDirection)
+        Dim interfaceType As Type = prop.PropertyType.GetInterface("IComparable")
+
+        If interfaceType Is Nothing AndAlso prop.PropertyType.IsValueType Then
+            Dim underlyingType As Type = Nullable.GetUnderlyingType(prop.PropertyType)
+
+            If Not (underlyingType Is Nothing) Then
+                interfaceType = underlyingType.GetInterface("IComparable")
+            End If
+        End If
+
+        If Not (interfaceType Is Nothing) Then
+            _sortPropertyValue = prop
+            _sortDirectionValue = direction
+
+            Dim query As IEnumerable(Of T) = MyBase.Items
+
+            If direction = ListSortDirection.Ascending Then
+                query = query.OrderBy(Function(i) prop.GetValue(i))
+            Else
+                query = query.OrderByDescending(Function(i) prop.GetValue(i))
+            End If
+
+            Dim newIndex As Integer = 0
+            For Each item As Object In query
+                Me.Items(newIndex) = DirectCast(item, T)
+                System.Math.Max(System.Threading.Interlocked.Increment(newIndex), newIndex - 1)
+            Next
+
+            _isSortedValue = True
+            Me.OnListChanged(New ListChangedEventArgs(ListChangedType.Reset, -1))
+        Else
+            Throw New NotSupportedException("Cannot sort by " + prop.Name + ". This" + prop.PropertyType.ToString() + " does not implement IComparable")
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' ソート対象
+    ''' </summary>
+    ''' <returns></returns>
+    Protected Overrides ReadOnly Property SortPropertyCore() As PropertyDescriptor
+        Get
+            Return _sortPropertyValue
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' ソート方向
+    ''' </summary>
+    ''' <returns></returns>
+    Protected Overrides ReadOnly Property SortDirectionCore() As ListSortDirection
+        Get
+            Return _sortDirectionValue
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' ソートのサポート
+    ''' 
+    ''' </summary>
+    ''' <returns></returns>
+    Protected Overrides ReadOnly Property SupportsSortingCore() As Boolean
+        Get
+            Return True
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' ソート済かどうか
+    ''' </summary>
+    ''' <returns></returns>
+    Protected Overrides ReadOnly Property IsSortedCore() As Boolean
+        Get
+            Return _isSortedValue
+        End Get
+    End Property
+
+#End Region
+
+End Class
